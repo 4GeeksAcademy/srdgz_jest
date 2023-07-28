c0a33eb7579cbd5076e146b027af2289dc122519
Sandra Rodríguez <133523307+srdgz@users.noreply.github.com>
2023-07-28 10:05:40 +0000

    Exercise done

---
+++ app.js
@@ -0,0 +1,28 @@
+const sum = (a,b) => {
+    return a + b;
+}
+console.log(sum(7,3));
+module.exports = { sum };
+
+
+let oneEuroIs = {
+    'JPY': 127.9,
+    'USD': 1.2,
+    'GBP': 0.8
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * oneEuroIs.USD;
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * (oneEuroIs.JPY / oneEuroIs.USD);
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * (oneEuroIs.GBP / oneEuroIs.JPY);
+    return Number(valueInPound.toFixed(3));
+}
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
